f5d05d922e1d2c98e072475b61c3274e48713fbe
daiyanggguang <you@example.com>
2017-02-24 00:00:33 +0800

    merge

---
+++ VC_LifeCircle/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  VC_LifeCircle
+//
+//  Created by 随随意 on 17/2/22.
+//  Copyright © 2017年 suisuiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
--- VC_LifeCircle/ViewController.m
+++ VC_LifeCircle/ViewController.m
@@ -14,9 +14,57 @@
 
 @implementation ViewController
 
+- (void)loadView {
+    [super loadView];
+    NSLog(@"%s", __FUNCTION__);
+
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"%s", __FUNCTION__);
+
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    NSLog(@"%s", __FUNCTION__);
+
+}
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    NSLog(@"%s", __FUNCTION__);
+}
+
+-(void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    NSLog(@"%s", __FUNCTION__);
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    NSLog(@"%s", __FUNCTION__);
+
+}
+
+- (void) viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    NSLog(@"%s", __FUNCTION__);
+
+    
+}
+
+- (void) viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    NSLog(@"%s", __FUNCTION__);
+
+}
+
+- (void) viewDidUnload {
+    [super viewDidUnload];
+    NSLog(@"%s", __FUNCTION__);
+
+    
 }
 
 
